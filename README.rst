b00478660a7c51e8c1fd09796953787ef4b9af0f
wtee <wtee2015@gmail.com>
2015-04-20 23:06:30 -0500

    Initial commit

---
+++ README.rst
@@ -0,0 +1,83 @@
+simple-slides.js
+================
+
+simple-slides.js is a small library I wrote after hacking 
+together a basic slideshow for a project. To save myself
+some hassle in future projects, I cleaned up the code and made
+it reusable.
+
+It's best suited for simple timelines, about us pages, and
+illustated narratives that can be split into slides. It's not a good
+Powerpoint replacement and it doesn't do automatic transitions.
+
+Features:
+---------
+* Simple to use
+* Vanilla javascript
+* Navigable with arrow keys
+* Supports using Tabltop.js, if that's you're thing
+* Looks decent across various viewport sizes (I plan on tweaking
+  the styling at some point to make it more fully responsive).
+* Small. 
+
+How to use it:
+-------------
+SimpleSlides was designed primarily for ease of use. With one line of
+javascript you can create a slideshow. Either like this::
+ SimpleSlides.initTabletop('http://url.to.your.google.spreadsheet', 200);
+Or like this::
+ SimpleSlides.init(array_with_slide_objects, 200);
+
+In both examples the first argument provides SimpleSlides with the
+data for the slides while the second argument is the height you want
+the slideshow container to be in pixels.
+
+Here's what a basic page would look like::
+  <html>
+    <head>
+      <title>A simple slideshow</title>
+      <link href="css/simple-slides.js"></script>
+      <script src="js/tabletop.js"></script>
+      <script src="js/simple-slides.js"></script>
+    </head>
+    <body>
+      <div id="slider-container"></div>
+      <script>
+        SimpleSlides.initTabletop('https://docs.google.com/spreadsheets/d/1llicGfdX7YVNxmPsdu3QbR04pBfPxk1DIX9irC31Iaw/pubhtml', 400);
+      </script>
+    </body>
+  </html>
+
+Here's `a demo with Tabletop`_ and a `demo without it`_.
+
+To use SimpleSlides include simple-slides.css and simple-slides.js in
+your document. If you want to use Tabletop with SimpleSlides include Tabletop.
+Create an empty div in the body of your document with the id "slider-container".
+Initialize SimpleSlides and voila.
+
+If you're using Tabletop, your Google spreadsheet will need to have these fields:
+* title
+* text
+* img
+* img_caption
+* link
+
+Here's `an example.`_
+
+If you're not using Tabletop, you'll need to create an array full of objects
+with the attribute names that match the spreadsheet fields above. For example::
+  var myarray = [ {title: 'My first slide',
+                   text: 'This text fills out the body of the slide',
+                   img: 'http://img.url',
+                   img_caption: 'A picture'
+                   link: 'http://for.more.info'}, // And so on ...
+
+License
+-------
+SimpleSlides is available under the terms of the `Unlicense`_, that is
+it's free and unencumbered software in the public domain.
+
+.. _a demo with Tabletop: http://www.sometimes-i.com/code/simple-slides/examples/with-tabletop.html
+.. _demo without it: http://www.sometimes-i.com/code/simple-slides/examples/without-tabletop.html
+.. _an example: https://docs.google.com/spreadsheets/d/1llicGfdX7YVNxmPsdu3QbR04pBfPxk1DIX9irC31Iaw/pubhtml
+.. _Unlicense: http://unlicense.org
---
+++ simple-slides.js
@@ -0,0 +1,93 @@
+"use strict"
+var SimpleSlides = {
+    _currentSelection: 0,
+    _previousSelection: 1,
+    _max: 0,
+    _height: 200,
+    _arrow: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAAAsUlEQVRYw93XSwqDMBCA4byPItITSpGCiEhP6aqIJ+iyTiHdCJJuhj9tIMzGxceo8zCmcEIIQ+kZ1WOtfcq9owAJL+ccg/gAMmJGAQjiCMiICQW8r/d+RAE5E/oIASxngJwJ3ToRY2wkPH4BcdNGtBLWAqJXRaSUeIQ0p28Q1xoyoY64SNgKiO4/AegrQD9C9DdECxFaivFmhLfjGgaS+kYydChFx3J0MUFXM3Q51V7Pd5XzyZvlE+cnAAAAAElFTkSuQmCC',
+
+    initTabletop: function (url, height) {
+        // Requires Tabletop.js, https://github.com/jsoma/tabletop
+        SimpleSlides._height = height;
+        Tabletop.init({key: url,
+                       callback: getData,
+                       simpleSheet: true,
+                       parseNumbers: true});
+        function getData(data) {SimpleSlides.init(data, SimpleSlides._height);}
+    },
+
+    init: function (arr, height) {
+        // All the heavy lifting: create the slides, add them to the DOM, add event listeners
+        var i = 0,
+            slider = document.getElementById('slider-container'),
+            frag = document.createDocumentFragment();
+        slider.style.height = height;
+        this._max = arr.length;
+ 
+         slider.insertAdjacentHTML('afterbegin', '<div id="prev" class="prev-next"><img src="' 
+                                   + this._arrow 
+                                   + '" alt="previous" /></div><div id="slides"></div><div id="next" class="prev-next"><img src="' 
+                                   + this._arrow 
+                                   + '" alt="next" />');
+        for (i; i < this._max; i++) {
+            var slide = document.createElement('div');
+
+            slide.className = 'slide';
+            slide.id= i;
+            slide.insertAdjacentHTML('afterbegin', '<img src="' 
+                                     + arr[i].img 
+                                     +'" title="' 
+                                     + arr[i].img_caption 
+                                     + '" /><div><h2>' 
+                                     + arr[i].title 
+                                     + '</h2><p class="details">' 
+                                     + arr[i].text + '</p></div>');
+            frag.appendChild(slide);  
+        }
+        console.log(frag);
+        document.getElementById('slides').appendChild(frag);
+        this.focusSlide(SimpleSlides._currentSelection, SimpleSlides._previousSelection);
+        document.getElementById('prev').addEventListener('click', SimpleSlides.prevSlide);
+        document.getElementById('next').addEventListener('click', SimpleSlides.nextSlide);
+        window.addEventListener('keydown', SimpleSlides.handleArrowKey);
+    },
+
+    focusSlide: function (curr, prev) {
+        // Bring a new slide in to view, move the previous one out of view
+        var c = document.getElementById(curr),
+            p = document.getElementById(prev),
+            height = document.getElementById('slider-container').offsetHeight;
+        c.style.left = 0;
+        c.style.top = -(height + 10) * curr + 'px';
+        p.style.left = '9000px';
+    },
+
+    prevSlide: function () {
+        // Bring the previous slide into focus if we're not at the first one
+        if (SimpleSlides._currentSelection > 0) {
+            SimpleSlides._previousSelection = SimpleSlides._currentSelection;
+            SimpleSlides._currentSelection -= 1;
+            SimpleSlides.focusSlide(SimpleSlides._currentSelection, SimpleSlides._previousSelection);
+        }
+    },
+
+    nextSlide: function () {
+        // Bring the next slide into focus if we're not at the last one
+        if (SimpleSlides._currentSelection < SimpleSlides._max - 1) {
+            SimpleSlides._previousSelection = SimpleSlides._currentSelection;
+            SimpleSlides._currentSelection += 1;
+            SimpleSlides.focusSlide(SimpleSlides._currentSelection, SimpleSlides._previousSelection);
+        }
+    },
+
+    handleArrowKey: function (e) {
+        // Allow the user to navigate the slideshow using the left and right arrow keys
+        if (e.keyCode === 39) {
+            SimpleSlides.nextSlide(e);
+        } 
+
+        if (e.keyCode === 37) {
+            SimpleSlides.prevSlide(e);
+        } 
+    }
+};
